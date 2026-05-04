bad4dcf4f0fe2752386a89d0dfac1973989f37d0
Li-Yu Lin <liyu8561501@gmail.com>
2026-05-04 19:08:29 -0400

    update backstepping
    Signed-off-by: Li-Yu Lin <liyu8561501@gmail.com>

--- scripts/rdd2_sim.py
+++ scripts/rdd2_sim.py
@@ -303,7 +303,7 @@ class Simulator(Node):
                 self.vw_sp,
                 self.qc_sp,
             )
-            [self.thrust, self.z_i, omega, self.q_sp] = self.eqs["se23_control"](
+            [self.thrust, self.z_i, self.q_sp] = self.eqs["se23_control"](
                 self.thrust_trim,
                 self.BK,
                 self.ki,
