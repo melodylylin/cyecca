a84def2fb23ceb4a559ab3d2d2b3994d2bb8d7e3
Li-Yu Lin <liyu8561501@gmail.com>
2026-05-08 07:04:55 -0400

    backup desktop
    Signed-off-by: Li-Yu Lin <liyu8561501@gmail.com>

---
+++ notebook/ins/bezier.py
@@ -0,0 +1,315 @@
+import casadi as ca
+from scipy.special import binom
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+from zipfile import ZipFile
+import os
+import datetime
+# from .TimeOptBez import *
+# from cyecca.lie.SE23 import *
+from cyecca.models.bezier import derive_ref
+import cyecca.lie as lie
+
+f_ref = derive_ref()["f_ref"]
+
+class Bezier:
+#https://en.wikipedia.org/wiki/B%C3%A9zier_curve
+
+    def __init__(self, P: ca.SX, T: float):
+        self.P = P
+        self.m = P.shape[0]
+        self.n = P.shape[1]-1
+        self.T = T
+    
+    def eval(self, t):
+        #https://en.wikipedia.org/wiki/De_Casteljau%27s_algorithm
+        beta = t/self.T
+        A = ca.SX(self.P)
+        for j in range(1, self.n + 1):
+            for k in range(self.n + 1 - j):
+                A[:, k] = A[:, k] * (1 - beta) + A[:, k + 1] * beta
+        return A[:, 0]
+    
+    def deriv(self, m=1):
+        D = ca.SX(self.P)
+        for j in range(0, m):
+            D = (self.n - j)*ca.horzcat(*[ D[:, i+1] - D[:, i] for i in range(self.n - j) ])
+        return Bezier(D/self.T**m, self.T)
+
+def derive_bezier7():
+    n = 8
+    T = ca.SX.sym('T')
+    t = ca.SX.sym('t')
+    P = ca.SX.sym('P', 1, n)
+    B = Bezier(P, T)
+
+    # derivatives
+    B_d = B.deriv()
+    B_d2 = B_d.deriv()
+    B_d3 = B_d2.deriv()
+    B_d4 = B_d3.deriv()
+
+    # boundary conditions
+
+    # trajectory
+    p = B.eval(t)
+    v = B_d.eval(t)
+    a = B_d2.eval(t)
+    j = B_d3.eval(t)
+    s = B_d4.eval(t)
+    r = ca.vertcat(p, v, a, j, s)
+
+    # given position/velocity boundary conditions, solve for bezier points
+    wp_0 = ca.SX.sym('p0', 4, 1)  # pos/vel at waypoint 0
+    wp_1 = ca.SX.sym('p1', 4, 1)  # pos/vel at waypoint 1
+
+    constraints = []
+    constraints += [(B.eval(0), wp_0[0])]  # pos @ wp0
+    constraints += [(B_d.eval(0), wp_0[1])]  # vel @ wp0
+    constraints += [(B_d2.eval(0), wp_0[2])]  # accel @ wp0
+    constraints += [(B_d3.eval(0), wp_0[3])]  # jerk @ wp0
+    constraints += [(B.eval(T), wp_1[0])]  # pos @ wp1
+    constraints += [(B_d.eval(T), wp_1[1])]  # vel @ wp1
+    constraints += [(B_d2.eval(T), wp_1[2])]  # accel @ wp1
+    constraints += [(B_d3.eval(T), wp_1[3])]  # jerk @ wp1
+
+    assert len(constraints) == n
+
+    Y = ca.vertcat(*[c[0] for c in constraints])
+    b = ca.vertcat(*[c[1] for c in constraints])
+    A = ca.jacobian(Y, P)
+    A_inv = ca.inv(A)
+    P_sol = (A_inv@b).T
+
+    functions = [
+        ca.Function('bezier7_solve', [wp_0, wp_1, T], [P_sol],
+            ['wp_0', 'wp_1', 'T'], ['P']),
+        ca.Function('bezier7_traj', [t, T, P], [r],
+            ['t', 'T', 'P'], ['r']),
+    ]
+
+    return { f.name(): f for f in functions }
+
+def derive_bezier3():
+    n = 4
+    T = ca.SX.sym('T')
+    t = ca.SX.sym('t')
+    P = ca.SX.sym('P', 1, n)
+    B = Bezier(P, T)
+
+    # derivatives
+    B_d = B.deriv()
+    B_d2 = B_d.deriv()
+    B_d3 = B_d2.deriv()
+    B_d4 = B_d3.deriv()
+
+    # boundary conditions
+
+    # trajectory
+    p = B.eval(t)
+    v = B_d.eval(t)
+    a = B_d2.eval(t)
+    r = ca.vertcat(p, v, a)
+
+    # given position/velocity boundary conditions, solve for bezier points
+    wp_0 = ca.SX.sym('p0', 2, 1)  # pos/vel at waypoint 0
+    wp_1 = ca.SX.sym('p1', 2, 1)  # pos/vel at waypoint 1
+
+    constraints = []
+    constraints += [(B.eval(0), wp_0[0])]  # pos @ wp0
+    constraints += [(B_d.eval(0), wp_0[1])]  # vel @ wp0
+    constraints += [(B.eval(T), wp_1[0])]  # pos @ wp1
+    constraints += [(B_d.eval(T), wp_1[1])]  # vel @ wp1
+
+    assert len(constraints) == n
+
+    Y = ca.vertcat(*[c[0] for c in constraints])
+    b = ca.vertcat(*[c[1] for c in constraints])
+    A = ca.jacobian(Y, P)
+    A_inv = ca.inv(A)
+    P_sol = (A_inv@b).T
+
+    functions = [
+        ca.Function('bezier3_solve', [wp_0, wp_1, T], [P_sol],
+            ['wp_0', 'wp_1', 'T'], ['P']),
+        ca.Function('bezier3_traj', [t, T, P], [r],
+            ['t', 'T', 'P'], ['r']),
+    ]
+
+    return { f.name(): f for f in functions }
+
+
+
+# def multirotor_timeOpt(bc,k_time): ## Currently outputs optimized time
+#     time_opt = find_opt_time(6, bc,k_time)
+#     return np.average(time_opt)
+
+def multirotor_plan(bc,T0, dt):
+    bezier_7 = derive_bezier7()
+
+    bc = np.array(bc)
+    t0 = np.arange(0, T0, dt)
+
+    PX = bezier_7['bezier7_solve'](bc[:, 0, 0], bc[:, 1, 0], T0)
+    traj_x = np.array(bezier_7['bezier7_traj'](np.array([t0]), T0, PX)).T
+
+    PY = bezier_7['bezier7_solve'](bc[:, 0, 1], bc[:, 1, 1], T0)
+    traj_y = np.array(bezier_7['bezier7_traj'](np.array([t0]), T0, PY)).T
+
+    PZ = bezier_7['bezier7_solve'](bc[:, 0, 2], bc[:, 1, 2], T0)
+    traj_z = np.array(bezier_7['bezier7_traj'](np.array([t0]), T0, PZ)).T
+
+    # V = np.sqrt(vx**2 + vy**2)bc_t = np.array([
+    #         [ # position
+    #         [0, 0, 0],  
+    #         [0, 0, 2],    
+    #         [0, 0, 5], 
+    #         [120, -3.05, 5],
+    #         [160, -5.2, 5],
+    #         [229.7, -9.6, 4],
+    #         [229.7, -9.6, 4],
+    #         [229.7, -9.6, 2],
+    #         ],
+    #         [ # velocity
+    #         [0, 0, 0.1],  
+    #         [0, 0, 0.1], 
+    #         [0, 0, 0],
+    #         [2.2, -0.1, 0],
+    #         [2.2, -0.1, 0],
+    #         [2.2, -0.1, 0],
+    #         [0, 0, 0],  # wp0, x, y, z
+    #         [0, 0, 0],
+    #         ],
+    #         [ # accel
+    #         [0, 0, 0], 
+    #         [0, 0, 0],
+    #         [0, 0, 0],
+    #         [0, 0, 0],
+    #         [0, 0, 0],
+    #         [0, 0, 0],
+    #         [0, 0, 0],
+    #         [0, 0, 0],
+    #         ],
+    #         [ # jerk
+    #         [0, 0, 0],
+    #         [0, 0, 0],
+    #         [0, 0, 0],
+    #         [0, 0, 0],
+    #         [0, 0, 0],
+    #         [0, 0, 0],
+    #         [0, 0, 0],
+    #         [0, 0, 0],
+    #         ]])
+
+    return PX, PY, PZ, traj_x, traj_y, traj_z, t0
+
+def generate_path(bc_t, dt):
+    
+    t_total = 0
+    res = {
+        'anchor_x': [],
+        'anchor_y': [],
+        'anchor_z': [],
+        'x': [],
+        'y': [],
+        'z': [],
+        'v_x': [],
+        'v_y': [],
+        'v_z': [],
+        'acc_x': [],
+        'acc_y': [],
+        'acc_z': [],
+        'theta0': [],
+        'theta1': [],
+        'theta2': [],
+        'T': [],
+        'T0': [],
+        'omega_1': [],
+        'omega_2': [],
+        'omega_3': []
+    }
+
+    for i in range(bc_t.shape[1]-1):
+        bc = bc_t[:,i:i+2,:]
+        T0 = 10
+        Px, Py, Pz, traj_x, traj_y, traj_z, t0 = multirotor_plan(bc, T0, dt)
+        t = t_total + t0
+        t_total = t_total + T0 
+        x = traj_x[:, 0]
+        y = traj_y[:, 0]
+        z = traj_z[:, 0]
+        vx = traj_x[:, 1]
+        vy = traj_y[:, 1]
+        vz = traj_z[:, 1]
+        ax = traj_x[:, 2]
+        ay = traj_y[:, 2]
+        az = traj_z[:, 2]-9.8
+        jx = traj_x[:, 3]
+        jy = traj_y[:, 3]
+        jz = traj_z[:, 3]
+        sx = traj_x[:, 4]
+        sy = traj_y[:, 4]
+        sz = traj_z[:, 4]
+
+        romega1 = []
+        romega2 = []
+        romega3 = []
+        rtheta0 = []
+        rtheta1 = []
+        rtheta2 = []
+    
+        for j in range(x.shape[0]):
+            r_vx = vx[j]
+            r_vy = vy[j]
+            r_vz = vz[j]
+            r_ax = ax[j]
+            r_ay = ay[j]
+            r_az = az[j]
+            r_jx = jx[j]
+            r_jy = jy[j]
+            r_jz = jz[j]
+            r_sx = sx[j]
+            r_sy = sy[j]
+            r_sz = sz[j]
+            ref_v = f_ref(0, 0, 0, [r_vx, r_vy, r_vz], [r_ax, r_ay, r_az], [r_jx, r_jy, r_jz], [r_sx, r_sy, r_sz])
+            quat = lie.SO3Quat.elem(ref_v[1])
+            theta = ca.DM(lie.SO3Mrp.from_Quat(quat).param)
+            theta = np.array(theta).reshape(3,)
+            r_theta0 = theta[0]
+            r_theta1 = theta[1]
+            r_theta2 = theta[2]
+            rtheta0.append(r_theta0)
+            rtheta1.append(r_theta1)
+            rtheta2.append(r_theta2)
+            omega = ref_v[2]
+            omega = np.array(omega).reshape(3,)
+            r_omega1 = omega[0]
+            r_omega2 = omega[1]
+            r_omega3 = omega[2]
+            romega1.append(r_omega1)
+            romega2.append(r_omega2)
+            romega3.append(r_omega3)
+
+        res['anchor_x'].extend(np.array(Px).tolist())
+        res['anchor_y'].extend(np.array(Py).tolist())
+        res['anchor_z'].extend(np.array(Pz).tolist())
+        res['x'].extend(x)
+        res['y'].extend(y)
+        res['z'].extend(z)
+        res['theta0'].extend(np.array(rtheta0).tolist())
+        res['theta1'].extend(np.array(rtheta1).tolist())
+        res['theta2'].extend(np.array(rtheta2).tolist())
+        res['v_x'].extend(vx)
+        res['v_y'].extend(vy)
+        res['v_z'].extend(vz)
+        res['acc_x'].extend(ax)
+        res['acc_y'].extend(ay)
+        res['acc_z'].extend(az)
+        res['T'].extend(np.array(t).tolist())
+        res['T0'].append(T0)
+        res['omega_1'].extend(np.array(romega1).tolist())
+        res['omega_2'].extend(np.array(romega2).tolist())
+        res['omega_3'].extend(np.array(romega3).tolist())
+
+    return res
--- notebook/ins/ins.py
+++ notebook/ins/ins.py
@@ -1,11 +1,17 @@
 import casadi as ca
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
 
 import cyecca.lie as lie
 from cyecca.graph import draw_casadi
 from cyecca.util import rk4, count_ops
 from pathlib import Path
+from bezier import generate_path
+import matplotlib as mpl
+
+mpl.rcParams['font.size'] = 14
+mpl.rcParams['text.usetex'] = True
 
 
 def derive_strapdown_ins_propagation():
@@ -84,27 +90,34 @@ f_integrate_rk4 = derive_integrate_rk4()
 f_integrate_rk4([0, 0, 0, 0, 0, 0, 0, 0, 0], [0.1, 0.2, 0.3], [0.1, 0.2, 0.3], 9.8, 0.1)
 
 
-def integrate_rk4(x0, a_b, omega_b, g, dt, tf):
+def integrate_rk4(x0, a_b, omega_b, g, dt, tf): # a_b, omega_b are measurements, add noise there
     t_list = np.arange(0, tf, dt)
     data = {"t": t_list, "x": np.zeros((len(t_list), len(x0)), dtype=float)}
     x = data["x"]
     x[0, :] = x0
+    # print(a_b.shape)
+    # print(omega_b.shape)
+    # print(len(t_list))
     for i in range(1, len(t_list)):
-        x[i, :] = np.array(f_integrate_rk4(x[i - 1, :], a_b, omega_b, g, dt)).reshape(
+        # print("a", a_b[:,i-1])
+        # print("omega", omega_b[:,i-1])
+        x[i, :] = np.array(f_integrate_rk4(x[i - 1, :], a_b[:, i-1], omega_b[:, i-1], g, dt)).reshape(
             -1
         )
     return data
 
 
-def integrate_mixed_invariant(x0, a_b, omega_b, g, dt, tf):
+def integrate_mixed_invariant(x0, a_b, omega_b, g, dt, tf): # a_b, omega_b are measurements, add noise there
     t_list = np.arange(0, tf, dt)
     data = {"t": t_list, "x": np.zeros((len(t_list), len(x0)), dtype=float)}
     x = data["x"]
 
     x[0, :] = x0
     for i in range(1, len(t_list)):
+        # print("mixed a", a_b[:, i-1])
+        # print("mixed omega", omega_b[:, i-1])
         x[i, :] = np.array(
-            f_strapdown_ins_propagation(x0, a_b, omega_b, g, t_list[i])
+            f_strapdown_ins_propagation(x[i-1, :], a_b[:, i], omega_b[:, i], g, dt) # change a_b and omega_b to a_b[i] and omega_b[i]
         ).reshape(-1)
     return data
 
@@ -251,11 +264,14 @@ def find_flops():
     plt.savefig("fig/ops.png")
 
 
-def comparison_tolerance(name: str, x0: np.array, a_b: np.array, omega_b: np.array):
+def comparison_tolerance(name: str, file_name: str):
+    with open(file_name, 'br') as f:
+        data = pickle.load(f)
+
     plt.close()
     fig_path = Path("fig")
     fig_path.mkdir(exist_ok=True)
-    tf = 10
+    tf = data['T']
     g = 9.8
 
     rk4_steps = [10 ** (-n) for n in range(1, 4)]
@@ -271,28 +287,72 @@ def comparison_tolerance(name: str, x0: np.array, a_b: np.array, omega_b: np.arr
     plt.grid()
 
     plt.figure(2)
+    # plt.title(
+    #    f"{name:s} Error in Runge-Kutta 4th Order Method"
+    # )
+    plt.ylabel("$log_{10} |e|$")
+    plt.xlabel("t, sec")
+    plt.grid()
+
+    plt.figure(3)
     # plt.title(f"Trajectory")
+    plt.plot(data['traj'][:, 0], data['traj'][:, 1], color='m', label='True')
     plt.xlabel("x, m")
     plt.ylabel("y, m")
     plt.grid()
-    plt.axis("equal")
-
-    plt.figure(3)
-    plt.title("mrp")
 
     plt.figure(4)
-    plt.title("velocity")
+    plt.title("mrp")
 
     plt.figure(5)
+    plt.title("velocity")
+
+    plt.figure(6)
     plt.title("position")
 
-    for step in rk4_steps:
+    plt.figure(7)
+    plt.plot(data['traj'][:, 0], data['traj'][:, 1], color='m', label='True')
+    plt.xlabel("x, m")
+    plt.ylabel("y, m")
+    plt.grid()
+    # plt.axis("equal")
+
+    fig = plt.figure(8,figsize=(8,8))
+    ax = fig.add_subplot(111, projection="3d")
+    ax.plot(data['traj'][:, 0], data['traj'][:, 1], data['traj'][:, 2], label='True', color='m')
+
+
+    fig2 = plt.figure(9,figsize=(8,8))
+    ax2 = fig2.add_subplot(111, projection="3d")
+    ax2.plot(data['traj'][:, 0], data['traj'][:, 1], data['traj'][:, 2], label='True', color='m')
+
+    ls = ['-.', (0, (3, 15, 1, 15, 1, 15)), '-.']
+    lw = [1.5, 1.5, 1.5]
+    c = ['g', 'c', 'r']
+    ls3d = ['-.', (0, (5, 8, 1, 8, 1, 8)), '-.']
+
+    for i, step in enumerate(rk4_steps):
+        print(i)
+        int_step = int(step/1e-3)
+        a_b = data['a_b'][:, ::int_step]
+        omega_b = data['omega_b'][:, ::int_step]
+        ref = data['traj'][::int_step, :]
+        x0 = data['traj'][0, :]
+
+        print(a_b.shape)
         data_rk4 = integrate_rk4(x0=x0, a_b=a_b, omega_b=omega_b, g=g, dt=step, tf=tf)
         data_mixed = integrate_mixed_invariant(
             x0=x0, a_b=a_b, omega_b=omega_b, g=g, dt=step, tf=tf
         )
+
+        e_rk4 = np.linalg.norm(data_rk4["x"][:, 0:3] - ref[:, 0:3], axis=1)
+        e_rk4 = np.where(e_rk4 == 0, 1e-15, e_rk4)
+        log_e_rk4 = np.log10(np.abs(e_rk4))
+        e_mixed = np.linalg.norm(data_mixed["x"][:, 0:3] - ref[:, 0:3], axis=1)
+        e_mixed = np.where(e_mixed == 0, 1e-15, e_mixed)
+        log_e_mixed = np.log10(np.abs(e_mixed))
         e = np.linalg.norm(data_rk4["x"] - data_mixed["x"], axis=1)
-        e = np.where(e == 0, 1e-15, e)
+        e = np.where(e==0, 1e-15, e)
         log_e = np.log10(np.abs(e))
 
         # plt.subplot(121)
@@ -303,14 +363,22 @@ def comparison_tolerance(name: str, x0: np.array, a_b: np.array, omega_b: np.arr
 
         # plt.subplot(122)
         plt.figure(1)
-        plt.plot(data_rk4["t"], log_e, label=f"step size: {step:10g}")
+        plt.plot(data_rk4["t"], log_e_rk4, label=f"step size: {step:10g}", linewidth=lw[i])
 
         plt.figure(2)
-        plt.plot(
-            data_rk4["x"][:, 0], data_rk4["x"][:, 1], label=f"step size: {step:10g}"
-        )
+        plt.plot(data_mixed["t"], log_e_mixed, label=f"step size: {step:10g}", linewidth=lw[i])
 
         plt.figure(3)
+        if i==2:
+            plt.plot(data_rk4["x"][:, 0], data_rk4["x"][:, 1], label=f"step size: {step:10g}", linestyle=ls[i], linewidth=lw[i], color=c[i], dashes=[2,15])
+        else:
+            plt.plot(data_rk4["x"][:, 0], data_rk4["x"][:, 1], label=f"step size: {step:10g}", linestyle=ls[i], linewidth=lw[i], color=c[i])
+        
+        # plt.plot(
+        #     data_rk4["x"][:, 0], data_rk4["x"][:, 1], label=f"step size: {step:10g}", linestyle=ls[i], linewidth=lw[i], color=c[i]
+        # )
+
+        plt.figure(4)
         plt.subplot(321)
         plt.plot(
             data_rk4["t"], data_rk4["x"][:, 6], label=f"roll rk4 step size: {step:10g}"
@@ -342,7 +410,7 @@ def comparison_tolerance(name: str, x0: np.array, a_b: np.array, omega_b: np.arr
             label=f"yaw mixed step size: {step:10g}",
         )
 
-        plt.figure(4)
+        plt.figure(5)
         plt.plot(
             data_rk4["t"], data_rk4["x"][:, 3], label=f"vx rk4 step size: {step:10g}"
         )
@@ -360,7 +428,7 @@ def comparison_tolerance(name: str, x0: np.array, a_b: np.array, omega_b: np.arr
             label=f"vy mixed step size: {step:10g}",
         )
 
-        plt.figure(5)
+        plt.figure(6)
         plt.plot(
             data_rk4["t"], data_rk4["x"][:, 0], label=f"px rk4 step size: {step:10g}"
         )
@@ -378,30 +446,316 @@ def comparison_tolerance(name: str, x0: np.array, a_b: np.array, omega_b: np.arr
             label=f"py mixed step size: {step:10g}",
         )
 
+        plt.figure(7)
+        if i==2:
+            plt.plot(data_mixed["x"][:, 0], data_mixed["x"][:, 1], label=f"step size: {step:10g}", linestyle=ls3d[i], linewidth=lw[i], color=c[i], dashes=[2,15])
+        else:
+            plt.plot(data_mixed["x"][:, 0], data_mixed["x"][:, 1], label=f"step size: {step:10g}", linestyle=ls3d[i], linewidth=lw[i], color=c[i])
+
+
+        if i==2:
+            ax.plot(data_rk4["x"][:, 0], data_rk4["x"][:, 1], data_rk4["x"][:, 2], label=f"step size: {step:10g}", linestyle=ls3d[i], linewidth=lw[i], color=c[i], dashes=[3,10])
+        else:
+            ax.plot(data_rk4["x"][:, 0], data_rk4["x"][:, 1], data_rk4["x"][:, 2], label=f"step size: {step:10g}", linestyle=ls3d[i], linewidth=lw[i], color=c[i])
+        ax.set_xlabel('x, m', labelpad=10)
+        ax.set_ylabel('y, m', labelpad=12)
+        ax.set_zlabel('z, m', rotation=90, labelpad=8)
+        ax.set_zlim(0, 0.1)
+        # ax.set_title('Reference Trajectory')
+        plt.axis('auto')
+        plt.tight_layout()
+
+        if i==2:
+            ax2.plot(data_mixed["x"][:, 0], data_mixed["x"][:, 1], data_mixed["x"][:, 2], label=f"step size: {step:10g}", linestyle=ls[i], linewidth=lw[i], color=c[i], dashes=[3,10])
+        else:
+            ax2.plot(data_mixed["x"][:, 0], data_mixed["x"][:, 1], data_mixed["x"][:, 2], label=f"step size: {step:10g}", linestyle=ls[i], linewidth=lw[i], color=c[i])
+        ax2.set_xlabel('x, m', labelpad=10)
+        ax2.set_ylabel('y, m', labelpad=12)
+        ax2.set_zlabel('z, m', rotation=90, labelpad=8)
+        ax2.set_zlim(0, 0.1)
+        # ax.set_title('Reference Trajectory')
+        plt.axis('auto')
+        plt.tight_layout()
+
+        plt.figure(10)
+        plt.plot(ref[:, 0], ref[:, 1])
+
     plt.figure(1)
-    plt.legend(loc="best")
-    plt.savefig(f"fig/{name:s}_tol.png")
+    plt.legend(loc="upper right")
+    plt.savefig(f"fig/{name:s}_rk4_tol.pdf", dpi=300)
     # plt.close(1)
 
     plt.figure(2)
-    plt.legend(loc="best")
-    plt.savefig(f"fig/{name:s}_traj.png")
-    # plt.close(2)
+    plt.legend(loc="upper right")
+    plt.savefig(f"fig/{name:s}_mixed_tol.pdf", dpi=300)
 
     plt.figure(3)
-    plt.legend(loc="best")
-    plt.savefig(f"fig/{name:s}_mrp.png")
-    plt.close(3)
+    plt.legend(loc="upper right")
+    plt.savefig(f"fig/{name:s}_rk4_traj.pdf", dpi=300)
+    # plt.close(3)
 
     plt.figure(4)
     plt.legend(loc="best")
-    plt.savefig(f"fig/{name:s}_4.png")
+    plt.savefig(f"fig/{name:s}_mrp.png")
     plt.close(4)
 
     plt.figure(5)
     plt.legend(loc="best")
-    plt.savefig(f"fig/{name:s}_5.png")
+    plt.savefig(f"fig/{name:s}_4.png")
     plt.close(5)
+
+    plt.figure(6)
+    plt.legend(loc="best")
+    plt.savefig(f"fig/{name:s}_5.png")
+    plt.close(6)
+
+    plt.figure(7)
+    plt.legend(loc="upper right")
+    # plt.plot(ref[:, 0], ref[:, 1])
+    plt.savefig(f"fig/{name:s}_mixed_traj.pdf", dpi=300)
+
+    plt.figure(8)
+    plt.legend(loc="upper right")
+    plt.savefig(f"fig/{name:s}_rk4_traj3d.pdf", dpi=300)
+
+    plt.figure(9)
+    plt.legend(loc="upper right")
+    plt.savefig(f"fig/{name:s}_mixed_traj3d.pdf", dpi=300)
+
+    plt.figure(10)
+
+    plt.show()
+    plt.close()
+
+
+def comparison_bz_tolerance(name: str, x0: np.array, bc_t: np.array):
+    plt.close()
+    fig_path = Path("fig")
+    fig_path.mkdir(exist_ok=True)
+    g = 9.8
+
+    rk4_steps = [10 ** (-n) for n in range(1, 4)]
+
+    plt.ioff()
+
+    plt.figure(1)
+    # plt.title(
+    #    f"{name:s} Error in Runge-Kutta 4th Order Method"
+    # )
+    plt.ylabel("$log_{10} |e|$")
+    plt.xlabel("t, sec")
+    plt.grid()
+
+    plt.figure(2)
+    # plt.title(
+    #    f"{name:s} Error in Runge-Kutta 4th Order Method"
+    # )
+    plt.ylabel("$log_{10} |e|$")
+    plt.xlabel("t, sec")
+    plt.grid()
+
+    plt.figure(3)
+    # plt.title(f"Trajectory")
+    plt.xlabel("x, m")
+    plt.ylabel("y, m")
+    plt.grid()
+
+    plt.figure(4)
+    plt.title("mrp")
+
+    plt.figure(5)
+    plt.title("velocity")
+
+    plt.figure(6)
+    plt.title("position")
+
+    plt.figure(7)
+    plt.grid()
+    plt.axis("equal")
+
+    fig = plt.figure(8)
+    ax = fig.add_subplot(111, projection="3d")
+
+    fig2 = plt.figure(9)
+    ax2 = fig2.add_subplot(111, projection="3d")
+
+    ls = ['-', '-.', '--']
+    lw = [1.5, 1.5, 1.5]
+    c = ['g', 'c', 'r']
+
+    for i, step in enumerate(rk4_steps):
+        ref = generate_path(bc_t, step)
+        a_b = np.array([ref['acc_x'], ref['acc_y'], ref['acc_z']])
+        omega_b = np.array([ref['omega_1'], ref['omega_2'], ref['omega_3']])
+        tf = np.sum(ref['T0'])
+        data_rk4 = integrate_rk4(x0=x0, a_b=a_b, omega_b=omega_b, g=g, dt=step, tf=tf)
+        data_mixed = integrate_mixed_invariant(
+            x0=x0, a_b=a_b, omega_b=omega_b, g=g, dt=step, tf=tf
+        )
+        print(np.array(ref["theta0"]).shape)
+        print(np.array(ref["x"]).shape)
+        e_rk4 = np.linalg.norm(data_rk4["x"] - np.array([ref["x"], ref["y"], ref["z"], ref["v_x"], ref["v_y"], ref["v_z"], ref["theta0"], ref["theta1"], ref["theta2"]]).T, axis=1)
+        e_mixed = np.linalg.norm(data_mixed["x"]- np.array([ref["x"], ref["y"], ref["z"], ref["v_x"], ref["v_y"], ref["v_z"], ref["theta0"], ref["theta1"], ref["theta2"]]).T, axis=1)
+        e_rk4 = np.where(e_rk4 == 0, 1e-15, e_rk4)
+        log_e_rk4 = np.log10(np.abs(e_rk4))
+        e_mixed = np.where(e_mixed == 0, 1e-15, e_mixed)
+        log_e_mixed = np.log10(np.abs(e_mixed))
+
+        # plt.subplot(121)
+        # plt.plot(data_rk4[step]['t'], e)
+        # plt.title(f'step {step:f}')
+        # plt.ylabel('e')
+        # plt.xlabel('t, s')
+
+        # plt.subplot(122)
+        plt.figure(1)
+        plt.plot(data_rk4["t"], log_e_rk4, label=f"step size: {step:10g}", linewidth=lw[i])
+
+        plt.figure(2)
+        plt.plot(data_mixed["t"], log_e_mixed, label=f"step size: {step:10g}", linewidth=lw[i])
+
+        plt.figure(3)
+        plt.plot(
+            data_rk4["x"][:, 0], data_rk4["x"][:, 1], label=f"step size: {step:10g}"
+        )
+
+        plt.figure(4)
+        plt.subplot(321)
+        plt.plot(
+            data_rk4["t"], data_rk4["x"][:, 6], label=f"roll rk4 step size: {step:10g}"
+        )
+        plt.subplot(323)
+        plt.plot(
+            data_rk4["t"], data_rk4["x"][:, 7], label=f"pitch rk4 step size: {step:10g}"
+        )
+        plt.subplot(325)
+        plt.plot(
+            data_rk4["t"], data_rk4["x"][:, 8], label=f"yaw rk4 step size: {step:10g}"
+        )
+        plt.subplot(322)
+        plt.plot(
+            data_mixed["t"],
+            data_mixed["x"][:, 6],
+            label=f"roll mixed step size: {step:10g}",
+        )
+        plt.subplot(324)
+        plt.plot(
+            data_mixed["t"],
+            data_mixed["x"][:, 7],
+            label=f"pitch mixed step size: {step:10g}",
+        )
+        plt.subplot(326)
+        plt.plot(
+            data_mixed["t"],
+            data_mixed["x"][:, 8],
+            label=f"yaw mixed step size: {step:10g}",
+        )
+
+        plt.figure(5)
+        plt.plot(
+            data_rk4["t"], data_rk4["x"][:, 3], label=f"vx rk4 step size: {step:10g}"
+        )
+        plt.plot(
+            data_rk4["t"], data_rk4["x"][:, 4], label=f"vy rk4 step size: {step:10g}"
+        )
+        plt.plot(
+            data_mixed["t"],
+            data_mixed["x"][:, 3],
+            label=f"vx mixed step size: {step:10g}",
+        )
+        plt.plot(
+            data_mixed["t"],
+            data_mixed["x"][:, 4],
+            label=f"vy mixed step size: {step:10g}",
+        )
+
+        plt.figure(6)
+        plt.plot(
+            data_rk4["t"], data_rk4["x"][:, 0], label=f"px rk4 step size: {step:10g}"
+        )
+        plt.plot(
+            data_rk4["t"], data_rk4["x"][:, 0], label=f"py rk4 step size: {step:10g}"
+        )
+        plt.plot(
+            data_mixed["t"],
+            data_mixed["x"][:, 0],
+            label=f"px mixed step size: {step:10g}",
+        )
+        plt.plot(
+            data_mixed["t"],
+            data_mixed["x"][:, 0],
+            label=f"py mixed step size: {step:10g}",
+        )
+
+        plt.figure(7)
+        plt.plot(
+            data_mixed["x"][:, 0], data_mixed["x"][:, 1], label=f"step size: {step:10g}"
+        )
+
+        if i==2:
+            ax.plot(data_rk4["x"][:, 0], data_rk4["x"][:, 1], data_rk4["x"][:, 2], label=f"step size: {step:10g}", linestyle=ls[i], linewidth=lw[i], color=c[i], dashes=[3,10])
+        else:
+            ax.plot(data_rk4["x"][:, 0], data_rk4["x"][:, 1], data_rk4["x"][:, 2], label=f"step size: {step:10g}", linestyle=ls[i], linewidth=lw[i], color=c[i])
+        ax.set_xlabel('x, m', labelpad=10)
+        ax.set_ylabel('y, m', labelpad=12)
+        ax.set_zlabel('z, m', rotation=90, labelpad=8)
+        # ax.set_title('Reference Trajectory')
+        plt.axis('auto')
+        plt.tight_layout()
+
+        if i==2:
+            ax2.plot(data_mixed["x"][:, 0], data_mixed["x"][:, 1], data_mixed["x"][:, 2], label=f"step size: {step:10g}", linestyle=ls[i], linewidth=lw[i], color=c[i], dashes=[3,10])
+        else:
+            ax2.plot(data_mixed["x"][:, 0], data_mixed["x"][:, 1], data_mixed["x"][:, 2], label=f"step size: {step:10g}", linestyle=ls[i], linewidth=lw[i], color=c[i])
+        ax2.set_xlabel('x, m', labelpad=10)
+        ax2.set_ylabel('y, m', labelpad=12)
+        ax2.set_zlabel('z, m', rotation=90, labelpad=8)
+        # ax.set_title('Reference Trajectory')
+        plt.axis('auto')
+        plt.tight_layout()
+
+        plt.figure(10)
+        plt.plot(ref["x"], ref["y"])
+
+    plt.figure(1)
+    plt.legend(loc="upper right")
+    plt.savefig(f"fig/{name:s}_tol.pdf", dpi=300)
+    # plt.close(1)
+
+    plt.figure(2)
+    plt.legend(loc="upper right")
+
+    plt.figure(3)
+    plt.legend(loc="best")
+    plt.savefig(f"fig/{name:s}_traj.png")
+    # plt.close(3)
+
+    plt.figure(4)
+    plt.legend(loc="best")
+    plt.savefig(f"fig/{name:s}_mrp.png")
+    plt.close(4)
+
+    plt.figure(5)
+    plt.legend(loc="best")
+    plt.savefig(f"fig/{name:s}_4.png")
+    plt.close(5)
+
+    plt.figure(6)
+    plt.legend(loc="best")
+    plt.savefig(f"fig/{name:s}_5.png")
+    plt.close(6)
+
+    plt.figure(7)
+
+    plt.figure(8)
+    plt.legend(loc="best")
+    plt.savefig(f"fig/{name:s}_rk4_traj.pdf", dpi=300)
+
+    plt.figure(9)
+    plt.legend(loc="upper right")
+    plt.savefig(f"fig/{name:s}_mixed_traj.pdf", dpi=300)
+
+    plt.figure(10)
 
     plt.show()
     plt.close()
